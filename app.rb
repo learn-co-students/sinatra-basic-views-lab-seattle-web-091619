792857fc9cd2eb4a87812db16d64b674c3c55f65
Packmanager9 <ColeJDitzler@gmail.com>
2019-10-08 10:58:48 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get "/alley" do
+
+        erb :index
+        end
+
+        get "/" do
+            erb :index
+        end
 
 end
